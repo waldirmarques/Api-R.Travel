f8e6661a336c67ce824e573868e85c0213acfdc6
WaldirMarques <waldirmarquesdesouza@gmail.com>
2020-01-15 11:45:27 -0300

    criando pacote para os arquivos de configuração do sistema

---
+++ src/main/java/br/com/Rtravel/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package br.com.Rtravel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+	public Docket detalheApi() {
+ 
+		Docket docket = new Docket(DocumentationType.SWAGGER_2);
+ 
+		docket
+		.select()
+		.apis(RequestHandlerSelectors.basePackage("br.com.Rtravel"))
+		.paths(PathSelectors.any())
+		.build()
+		.apiInfo(this.informacoesApi().build());
+ 
+		return docket;
+	}
+ 
+	private ApiInfoBuilder informacoesApi() {
+ 
+		ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
+ 
+		apiInfoBuilder.title("Api-Rtravel");
+		apiInfoBuilder.description("Api para realização de um CRUD.");
+		apiInfoBuilder.version("1.0");
+		apiInfoBuilder.termsOfServiceUrl("Termo de uso: Deve ser usada para estudos.");
+		apiInfoBuilder.license("Licença - Open Source");
+		apiInfoBuilder.licenseUrl("http://www.rtravel.com.br");
+		apiInfoBuilder.contact(this.contato());
+ 
+		return apiInfoBuilder;
+ 
+	}
+	private Contact contato() {
+ 
+		return new Contact(
+				"Waldir Marques",
+				"http://www.rtravel.com.br", 
+				"waldirmarques90@gmail.com");
+	}
+}
