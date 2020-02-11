9f5b5fc0aa022a8ec786697423ef5d9cf6a1027a
WaldirMarques <waldirmarquesdesouza@gmail.com>
2020-02-11 14:23:37 -0300

    removendo import que não está sendo usado

--- src/main/java/br/com/Rtravel/resources/RotaResource.java
+++ src/main/java/br/com/Rtravel/resources/RotaResource.java
@@ -8,7 +8,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
