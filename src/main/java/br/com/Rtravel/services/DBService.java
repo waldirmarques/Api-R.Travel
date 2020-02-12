416b025c572f64056af8d68feec1e88b50a1654d
emersondantas <emerson.ruan@dce.ufpb.br>
2020-02-12 16:39:21 -0300

    Added jp city

--- src/main/java/br/com/Rtravel/services/DBService.java
+++ src/main/java/br/com/Rtravel/services/DBService.java
@@ -51,7 +51,8 @@ public class DBService {
 	
 		Cidade cid1 = new Cidade(null,"Paraiba-PB","Rio Tinto", "83.0", "43.9");
 		Cidade cid2 = new Cidade(null,"Paraiba-PB","Baia da Traição", "56.0", "90.9");
-		cidadeRepository.saveAll(Arrays.asList(cid1,cid2));
+		Cidade cid3 = new Cidade(null,"Paraiba-PB","João Pessoa", "56.0", "90.9");
+		cidadeRepository.saveAll(Arrays.asList(cid1,cid2, cid3));
 		
 		Parada para1 = new Parada(null,"Perto do posto de Rio Tinto","https://www.google.com/search?q=posto+de+rio+tinto", "83.42", "42.23");
 		Parada para2 = new Parada(null,"Perto da honda","https://www.google.com/search?q=honda+de+rio+tinto", "83.32", "42.53");
