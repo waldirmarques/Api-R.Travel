ca35c18ed8a44b1a5b88cde5c99ef21f14c32247
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-03-10 19:22:24 -0300

    adicionar tag jsonIgnore

--- src/main/java/br/com/Rtravel/domaim/Usuario.java
+++ src/main/java/br/com/Rtravel/domaim/Usuario.java
@@ -11,9 +11,8 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import br.com.Rtravel.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -21,40 +20,39 @@ import lombok.ToString;
 
 @Data()
 @ToString(exclude = { "" })
-@EqualsAndHashCode(exclude = {"name", "email", "senha", "perfis"})
+@EqualsAndHashCode(exclude = { "name", "email", "senha", "perfis" })
 @Entity
 public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@NotEmpty(message="Preenchimento obrigatório")
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String nome;
 
-	@NotEmpty(message="Preenchimento obrigatório")
-	@Email(message="Email inválido")
-	@Column(unique=true)
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
+	@Column(unique = true)
 	private String email;
 
-	@NotEmpty(message="Preenchimento obrigatório")
+	@NotEmpty(message = "Preenchimento obrigatório")
 	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String senha;
 
-	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
 	@JsonIgnore
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
 	private List<Comentario> comentarios = new ArrayList<>();
-	
-	@ElementCollection(fetch=FetchType.EAGER) //Sempre que buscar um usuário também pega seu perfil
-	@CollectionTable(name="PERFIS") //Nome da tabela de ligação entre usuario e perfil
+
+	@ElementCollection(fetch = FetchType.EAGER) // Sempre que buscar um usuário também pega seu perfil
+	@CollectionTable(name = "PERFIS") // Nome da tabela de ligação entre usuario e perfil
 	private Set<Integer> perfis = new HashSet<>();
-		
-	
+
 	public Usuario() {
 		addPerfil(Perfil.ADMIN);
 	}
-	
+
 	public Usuario(Integer id, String nome, String email, String senha) {
 		super();
 		this.id = id;
@@ -63,7 +61,7 @@ public class Usuario implements Serializable {
 		this.senha = senha;
 		addPerfil(Perfil.ADMIN);
 	}
-	
+
 	public Set<Perfil> getPerfis() {
 		return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
 	}
