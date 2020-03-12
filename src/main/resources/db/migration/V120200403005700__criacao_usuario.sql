bdf44450cf920980b7997ab6e92860ba18782a48
emanoelmedeiros <emanoelmlsilva83@gmail.com>
2020-03-11 21:05:58 -0300

    remover migração

--- src/main/resources/db/migration/V120200403005700__criacao_usuario.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO public.usuario
-(email, nome, senha)
-VALUES('chico@gmail.com', 'Chico Bento', '$2a$10$g58iR2d/9.vKMUW90VoZuu9A.Bkoj1rdCJWZFClAS1sM17L98/jOa');
-
-
-INSERT INTO public.perfis
-(usuario_id, perfis)
-VALUES(1, 1);
